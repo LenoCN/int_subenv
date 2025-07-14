3dd093f508fd92668466150f8cd490f3086faa99
Leno <14288737+LenoCN@user.noreply.gitee.com>
2025-07-14 19:15:14 +0800

    feat: Add initial int_test_pkg and int_subenv_pkg

---
+++ int_subenv_pkg.sv
@@ -0,0 +1,11 @@
+`ifndef INT_SUBENV_PKG
+`define INT_SUBENV_PKG
+package int_subenv_pkg;
+    import uvm_pkg::*;
+    import soc_com_pkg::*;  
+    import bus_subenv_pkg::*;
+    `include "env/int_subenv.sv"
+    `include "seq/int_base_sequence.sv"
+endpackage
+
+`endif 
---
+++ test/int_test_pkg.sv
@@ -0,0 +1,20 @@
+`ifndef INT_TEST_PKG
+`define INT_TEST_PKG
+package int_test_pkg;
+    import uvm_pkg::*;
+    import svt_axi_uvm_pkg::*;
+    import soc_com_pkg::*;
+    import com_subenv_pkg::*;  
+    import bus_subenv_pkg::*;
+    import efuse_subenv_pkg::*;
+    import pad_subenv_pkg::*;
+    import crg_subenv_pkg::*;
+    import int_subenv_pkg::*;
+    
+    `include "int_tc_base.sv"
+    `include "tc_int_sanity.sv"
+    `include "tc_scp_rd_reg.sv"
+
+endpackage
+import int_test_pkg::*;
+`endif
